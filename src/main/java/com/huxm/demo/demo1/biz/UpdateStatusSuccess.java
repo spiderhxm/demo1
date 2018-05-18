56e42bc10a70f2d17e20fad56f99089f3cac6b36
huxm <huxm@allinfinance.com>
2018-05-18 19:28:39 +0800

    second commit

---
+++ src/main/java/com/huxm/demo/demo1/biz/UpdateStatusSuccess.java
@@ -0,0 +1,14 @@
+package com.huxm.demo.demo1.biz;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+
+public class UpdateStatusSuccess implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
