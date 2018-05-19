24e736a34be489bc72067797da80a45483098d17
huxm <huxm@allinfinance.com>
2018-05-19 09:58:39 +0800

    3rd

--- src/main/java/com/huxm/demo/demo1/biz/UpdateStatusSuccess.java
+++ src/main/java/com/huxm/demo/demo1/biz/UpdateStatusSuccess.java
@@ -5,9 +5,15 @@ import org.activiti.engine.delegate.JavaDelegate;
 
 public class UpdateStatusSuccess implements JavaDelegate {
 
+	
 	@Override
 	public void execute(DelegateExecution arg0) throws Exception {
 		// TODO Auto-generated method stub
+		
+		String bizKey = arg0.getProcessBusinessKey();
+		
+		
+		
 		
 	}
 
