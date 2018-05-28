23d5c974f598240aa24572a84e32f75b1fa70345
huxm <huxm@allinfinance.com>
2018-05-28 19:10:21 +0800

    5.28

---
+++ src/main/java/com/huxm/demo/demo1/Test2.java
@@ -0,0 +1,15 @@
+package com.huxm.demo.demo1;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		i++;
+		++i;
+		System.out.println("i="+i);
+		
+
+	}
+
+}
